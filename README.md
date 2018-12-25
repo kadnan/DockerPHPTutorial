b1133aac71c3d2fe4e820cf2a3fb2f5222b2175b
Adnan Siddiqi <kadnan@gmail.com>
2018-12-25 11:24:10 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Docker PHP Tutorial
+This repo is the part of the post [Create your first PHP/MySQL application in docker](http://blog.adnansiddiqi.me/getting-started-with-docker/?utm_source=docker_php_post&utm_medium=github&utm_campaign=c_docker_php_post)
---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+$conn = new mysqli("mysql", "root", ".sweetpwd.", "my_db");
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row['name']."<br>";
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
